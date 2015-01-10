d120887b46c1c2bb2c306eaa3e9f0c64ebec6327
avenda <avenda@gmail.com>
2015-01-10 14:59:38 -0300

    Adding LICENCE, update README.md and the module it self

---
+++ README.md
@@ -0,0 +1,1 @@
+Based on the angular-route-styles repo. This module for AngularJS that provides the ability to have route-specific JAVASCRIPT scripts, using `$routeProvider` service.
---
+++ route-scripts.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Victor Avendano on 1/10/15.
+ * avenda@gmail.com
+ */
+(function(){
+
+    var mod = angular.module('routeScripts', ['ngRoute']);
+
+    mod.directive('routeScripts', ['$rootScope','$compile',
+        function($rootScope, $compile){
+          return {
+            restrict: 'A',
+            link: function (scope, element) {
+              var html = '<script ng-src="{{jsUrl}}" ng-repeat="(routeCtrl, jsUrl) in routeScripts"></script>';
+              element.append($compile(html)(scope));
+              scope.routeScripts = {};
+              $rootScope.$on('$routeChangeStart', function (e, next, current) {
+                  if(current && current.$$route && current.$$route.js){
+                    if(!Array.isArray(current.$$route.js)){
+                        current.$$route.js = [current.$$route.js];
+                    }
+                    angular.forEach(current.$$route.js, function(script){
+                        delete scope.routeScripts[script];
+                    });
+                  }
+                  if(next && next.$$route && next.$$route.js){
+                    if(!Array.isArray(next.$$route.js)){
+                        next.$$route.js = [next.$$route.js];
+                    }
+                    angular.forEach(next.$$route.js, function(script){
+                        scope.routeScripts[script] = script;
+                    });
+                  }       
+              });
+            }
+          };          
+        }
+    ]);
+
+})();
